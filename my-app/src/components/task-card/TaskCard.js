36628d3e90e8257f9f4dc3743c430af9fd8aa67c
Emilia-Dumitru <dumitruemilia41@gmail.com>
2024-07-08 22:41:24 +0300

    am adaugat un buton de delete

--- my-app/src/components/task-card/TaskCard.js
+++ my-app/src/components/task-card/TaskCard.js
@@ -3,11 +3,26 @@ import Badge from "../badge/Badge";
 import DateContainer from "../date-container/DateContainer";
 
 function TaskCard(props) {
+    
+    function setUnfocused() {
+        props.markAsUnfocus();
+    }
+    function setFocused() {
+        props.markAsFocus();
+    }
+    const focusedClass = props.isFocused ? "focused" : "unfocused";
     return (
-        <div className="card-wrapper">
+        <div className={"card-wrapper " + focusedClass} onMouseEnter={setFocused} onMouseLeave={setUnfocused}>
+
+
             <div className="card-header">
                 <p className="task-id">{props.id}</p>
                 <Badge status={props.status} />
+                {
+                    props.isFocused ?
+                        <span className="delete-button" onClick={() => props.deleteTask(props.id)}>&times;</span>
+                        : ""
+                }
             </div>
             <div className="card-content">
                 <p>{props.name}</p>
--- my-app/src/components/task-container/TaskContainer.js
+++ my-app/src/components/task-container/TaskContainer.js
@@ -1,18 +1,38 @@
 import "./TaskContainer.css";
 import TaskCard from "../task-card/TaskCard.js";
+import { useState } from "react";
 
 function TaskContainer(props) {
+    function deleteTask(taskId) {
+        console.log("Se va sterge task-ul " + taskId);
+    }
+    const [focusTaskId, setFocusTaskId] = useState(null);
+    const setUnfocus = () => {
+        setFocusTaskId(null);
+    };
+
     return (
-        <div className="task-container">
-            {props.data.map((task, index) => (
-                <TaskCard
-                    key={task.id}
-                    id={task.id}
-                    status={task.status}
-                    name={task.name}
-                    dueDate={task.dueDate}
-                />
-            ))}
+        <div>
+            {
+                focusTaskId ?
+                    <div>Focused Task : {focusTaskId} </div>
+                    : <div>Unfocused Task</div>
+            }
+            <div className="task-container">
+                {props.data.map((task, index) => (
+                    <TaskCard
+                        isFocused={focusTaskId === task.id}
+                        markAsUnfocus={setUnfocus}
+                        markAsFocus={() => setFocusTaskId(task.id)}
+                        deleteTask={deleteTask}
+                        key={task.id}
+                        id={task.id}
+                        status={task.status}
+                        name={task.name}
+                        dueDate={task.dueDate}
+                    />
+                ))}
+            </div>
         </div>
     );
 }
