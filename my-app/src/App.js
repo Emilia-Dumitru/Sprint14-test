1d6dc9b12fffcecce85e1e389510ffa36bc571c7
Emilia-Dumitru <dumitruemilia41@gmail.com>
2024-07-13 20:30:39 +0300

    am imbunatatit codul pentru a crea un nou task

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,63 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import TaskContainer from './components/task-container/TaskContainer';
 import CreateTaskButton from './components/task-form/ButtonCreateTask';
+import TaskData from './components/task-data/TaskData';
 
 function App() {
+  const initialData = TaskData();
+  const [tasks, setTasks] = useState(initialData); 
 
-  const data = [
-    {
-      id: "T-1",
-      name: "Create a Design System for Enum Workspace.",
-      status: "Todo",
-      dueDate: new Date(2024, 5, 23),
-    },
-    {
-      id: "T-2",
-      name: "12 Create a Design System for Enum Workspace.",
-      status: "In Progress",
-      dueDate: new Date(2024, 7, 24),
-    },
-    {
-      id: "T-3",
-      name: "13 Create a Design System for Enum Workspace.",
-      status: "Completed",
-      dueDate: new Date(2024, 6, 13),
-    },
-    {
-      id: "T-4",
-      name: "14 Create a Design System for Enum Workspace.",
-      status: "Todo",
-      dueDate: new Date(2024, 5, 23),
-    },
-    {
-      id: "T-5",
-      name: "15 Create a Design System for Enum Workspace.",
-      status: "Completed",
-      dueDate: new Date(2024, 6, 13),
-    },
-    {
-      id: "T-6",
-      name: "Create a Design System for Enum Workspace.",
-      status: "Pending",
-      dueDate: new Date(2024, 5, 23),
-    },
-    {
-      id: "T-7",
-      name: "16 Create a Design System for Enum Workspace.",
-      status: "Todo",
-      dueDate: new Date(2024, 5, 23),
-    },
-    
-  ];
+  const addTask = (newTask) => {
+    setTasks([...tasks, newTask]);
+  };
+
   return (
     <div>
       <div className='control-panel-container'>
-      <h1>Task Manager</h1>
-      <CreateTaskButton/>
+        <h1>Task Manager</h1>
+        <CreateTaskButton addTask={addTask} />
       </div>
       <div className="app-container">
-        <TaskContainer data ={data}/> 
+        <TaskContainer data={tasks} />
       </div>
     </div>
   );
--- my-app/src/components/task-form/ButtonCreateTask.js
+++ my-app/src/components/task-form/ButtonCreateTask.js
@@ -3,22 +3,24 @@ import CreateTaskForm from "./TaskForm";
 import "./ButtonCreateTask.css";
 
 
-const CreateTaskButton = () => {
+const CreateTaskButton = ({ addTask }) => {
     const [showForm, setShowForm] = useState(false);
+
     const handleButtonClick = () => {
         setShowForm(!showForm);
     };
+
     const closeForm = () => {
         setShowForm(false);
     };
 
     return (
         <div className="btn-app">
-            <button className="open-form-button" onClick={handleButtonClick}>
-                Create Task
-            </button>
-            {showForm && <CreateTaskForm closeForm={closeForm} />}
-        </div>
+        <button className="open-form-button" onClick={handleButtonClick}>
+            Create Task
+        </button>
+        {showForm && <CreateTaskForm addTask={addTask} closeForm={closeForm} />}
+    </div>
     );
 }
 
